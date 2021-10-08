b2f0ed66d5a7b58984c7ecdf7be29e9c371bb902
Kekoa Kon <kakon@dons.usfca.edu>
2021-10-07 21:05:32 -0700

    finalize and test the use of the function

--- code/functions/convert_mobility_wide_to_long.R
+++ code/functions/convert_mobility_wide_to_long.R
@@ -8,34 +8,34 @@
 # September 29, 2021
 
 # create a function to convert the Apple mobility data from wide to long
-convert_mobility_data_wide_to_long <- function(state_data)
+convert_mobility_data_wide_to_long <- function(input_file_name) {
 
 # load the "dplyr" package
 library("dplyr")
 
 # read in subsetted Apple Mobility data
-input_file_name <- paste0("output/subsetted_data/",
-                          "applemobilitytrends-2021-09-16",
-                          "_",
-                          state_data,
-                          ".csv")
+state_data <- readr::read_csv(paste0("output/subsetted_data/",
+"applemobilitytrends-2021-09-22_California.csv"))
 
-# run dplyr to filter out the State column to only in
-state_data <- all_covid_data %>%
-  dplyr::filter(sub.region == "California")
+
+# use a tidyr package to convert the data from wide format to long
+state_data_long <- tidyr::pivot_longer(state_data, cols = starts_with("X202"),
+                                      names_to = "date",
+                    values_to = "relative_mobility")
+
 
 # save the long data set as a csv in the output directory
-readr::write_csv(, file = paste0("output/",
+readr::write_csv(state_data_long, file = paste0("output/",
                                  "subsetted_states_long/",
                                           tools::file_path_sans_ext(
                                             basename(input_file_name)),
                                           "_",
-                                          "convert_mobility_data_wide_to_long",
+                                          "wide_to_long",
                                           ".csv"))
+}
+# test out the function
+source("code/functions/convert_mobility_wide_to_long.R")
 
-
-
-
-
-
-
+convert_mobility_data_wide_to_long(paste0(
+  input_file_name =  "output/subsetted_data/",
+  "applemobilitytrends-2021-09-16_California.csv"))
